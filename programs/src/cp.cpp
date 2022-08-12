581a444e757778447796a9c249cb88cb8f88f505
deepak bamania <deepak.bamania@hahn-schickard.de>
2022-08-12 16:47:12 +0200

    more updates

--- programs/src/cp.cpp
+++ programs/src/cp.cpp
@@ -23,14 +23,12 @@
 #include <moveit_visual_tools/moveit_visual_tools.h>
 
 //Global Variables
-moveit_msgs::AttachedCollisionObject attached_objects[6]; 
+moveit_msgs::AttachedCollisionObject attached_objects[2]; 
 
-double ObjectCoordinates[6][7] = {
-  {-0.23, -0.16, 0.36,  0.707388, -0.706825, 0.0005629, 0.0005633}, {-0.430, -0.13875, 0.4646,  0, 0, 0, 1},
-  {-0.27, 0.23, 0.09,  0, 0, 0.7071068, -0.7071068}, {-0.23, -0.13, 0.1,  0, 0, 0,1}, 
-  {-0.27, -0.305, 0.08,  0, 0, -0.7071068, 0.7071068},     {-0.31, -0.27, 0.16,  0, 0, -0.7071068, 0.7071068}};
+double ObjectCoordinates[2][7] = {
+  {-0.23, -0.16, 0.36,  0.707388, -0.706825, 0.0005629, 0.0005633}, {-0.430, -0.13875, 0.4646,  0, 0, 0, 1}};
   
-std::string ObjectNames[6] = {"PlungerPlate","PressureLid","output_plate","filter_plate", "Waste_plate", "Sample_rack"};
+std::string ObjectNames[2] = {"box1","box2"};
 
 
 
@@ -46,7 +44,7 @@ void movebot(moveit::planning_interface::MoveGroupInterface& move_group,
     //move_group.setNumPlanningAttempts(100);
     moveit::planning_interface::MoveGroupInterface::Plan my_plan_arm;
     geometry_msgs::PoseStamped current_pose;
-    current_pose = move_group.getCurrentPose("gripper");
+    current_pose = move_group.getCurrentPose("wrist2");
     geometry_msgs::Pose target_pose1;
 
 
@@ -70,44 +68,18 @@ void movebot(moveit::planning_interface::MoveGroupInterface& move_group,
 
 
 
-//Function for operating gripper
-void gripper(moveit::planning_interface::MoveGroupInterface& grip, std::string status)
-{
-  moveit::planning_interface::MoveGroupInterface::Plan my_plan_gripper;
-  grip.setJointValueTarget(grip.getNamedTargetValues(status));
-  ROS_INFO("Gripper Target Value Reached");
-  grip.move();
-
-  ros::WallDuration(1).sleep();
-    
-}
-
-
+//Function for adding collision objects to the world
 void addobj(ros::Publisher& diff, moveit_msgs::PlanningScene& planning_scene)
 {
 
-  shape_msgs::Mesh mesh[6];
-  shapes::ShapeMsg mesh_msg[6];
-  shapes::Mesh* m[6]; 
-    
-  for (int i=0; i<6;i++)
+  
+  for (int i=0; i<2;i++)
   {
-    std::string filename = ObjectNames[i];
-    std::string path ="package://withgripper/objects/";
-    std::string extension = ".stl";
-    std::string pathname= path+ObjectNames[i]+extension;
-    
 
     attached_objects[i].object.header.frame_id = "world";
     attached_objects[i].object.id = ObjectNames[i];
 
-    Eigen::Vector3d b(1, 1, 1);
-    m[i] = shapes::createMeshFromResource(pathname, b);
-    shapes::constructMsgFromShape(m[i],mesh_msg[i]);
-    mesh[i] = boost::get<shape_msgs::Mesh>(mesh_msg[i]);
-
     geometry_msgs::Pose pose;
-
     pose.position.x = ObjectCoordinates[i][0];
     pose.position.y = ObjectCoordinates[i][1];
     pose.position.z = ObjectCoordinates[i][2];
@@ -116,8 +88,16 @@ void addobj(ros::Publisher& diff, moveit_msgs::PlanningScene& planning_scene)
     pose.orientation.z = ObjectCoordinates[i][5];
     pose.orientation.w = ObjectCoordinates[i][6];
 
-    attached_objects[i].object.meshes.push_back(mesh[i]);
-    attached_objects[i].object.mesh_poses.push_back(pose);
+    shape_msgs::SolidPrimitive primitive;
+    primitive.type = primitive.BOX;
+    primitive.dimensions.resize(3);
+    primitive.dimensions[0] = 0.02;
+    primitive.dimensions[1] = 0.02;
+    primitive.dimensions[2] = 0.02;
+
+    
+    attached_objects[i].object.primitives.push_back(primitive);
+    attached_objects[i].object.primitive_poses.push_back(pose);
     attached_objects[i].object.operation = attached_objects[i].object.ADD;
 
     planning_scene.world.collision_objects.push_back(attached_objects[i].object);
@@ -127,18 +107,20 @@ void addobj(ros::Publisher& diff, moveit_msgs::PlanningScene& planning_scene)
   }
   
 
-  ROS_INFO("All Object Added");
- 
-
-  
+  ROS_INFO("All Object Added");  
 }
 
+
+
+
+
+//Function for attaching collision objects to the robot
 void attachobj(ros::Publisher& diff, moveit_msgs::PlanningScene& planning_scene, int ObjectId)
 {
   
 
-  attached_objects[ObjectId].link_name = "gripper";
-  attached_objects[ObjectId].touch_links = std::vector<std::string>{ "clawl","clawr","gripper" };
+  attached_objects[ObjectId].link_name = "wrist2";
+  attached_objects[ObjectId].touch_links = std::vector<std::string>{ "wrist2" };
   
   moveit_msgs::CollisionObject remove_object;
   remove_object.id = ObjectNames[ObjectId];
@@ -153,11 +135,13 @@ void attachobj(ros::Publisher& diff, moveit_msgs::PlanningScene& planning_scene,
   diff.publish(planning_scene);
 }
 
+
+//Function for deattaching collision objects from the robot
 void detachobj(ros::Publisher& diff, moveit_msgs::PlanningScene& planning_scene, int ObjectId)
 {
   moveit_msgs::AttachedCollisionObject detach;
   detach.object.id = ObjectNames[ObjectId];
-  detach.link_name = "clawl";
+  detach.link_name = "wrist2";
   detach.object.operation = attached_objects[ObjectId].object.REMOVE;
   
   ROS_INFO("detach object from robot frame");
@@ -187,37 +171,23 @@ int main(int argc, char** argv)
   // MoveIt operates on sets of joints called "planning groups" and stores them in an object called
   // the `JointModelGroup`. Throughout MoveIt the terms "planning group" and "joint model group"
   // are used interchangably.
-  static const std::string PLANNING_GROUP_ARM = "robot";
-  static const std::string PLANNING_GROUP_GRIPPER = "hand";
-  static const std::string PLANNING_GROUP_ROBOT = "robot_hand";
-    
+  static const std::string PLANNING_GROUP_ARM = "mrdummy";
     // The :planning_interface:`MoveGroupInterface` class can be easily
     // setup using just the name of the planning group you would like to control and plan for.
   moveit::planning_interface::MoveGroupInterface move_group_interface_arm(PLANNING_GROUP_ARM);
-  moveit::planning_interface::MoveGroupInterface move_group_interface_gripper(PLANNING_GROUP_GRIPPER);
-  moveit::planning_interface::MoveGroupInterface move_group_interface_robot(PLANNING_GROUP_ROBOT);
   moveit_msgs::PlanningScene ps;
 
 
 
   addobj(planning_scene_diff_publisher, ps);  
   
-  //gripper(move_group_interface_gripper, "open");
   
-  //movebot(move_group_interface_arm, -0.36625, 0.15825, 0.508, 0.5, -0.5, 0.5, -0.5);
-  //movebot(move_group_interface_arm, -0.3755, 0.15825, 0.4845, 0.5, -0.5, 0.5, -0.5);
+  movebot(move_group_interface_arm, 0.079115, 0.0562584, 0.155379, 0.026604, 0.0127926, 0.43747, 0.898747);
   //attachobj(planning_scene_diff_publisher,ps,0);
-  //gripper(move_group_interface_gripper, "close");
-  //movebot(move_group_interface_arm, -0.3755, 0.15825, 0.508, 0.5, -0.5, 0.5, -0.5);
-  //move_group_interface_arm.setJointValueTarget(move_group_interface_arm.getNamedTargetValues("home"));
-  //move_group_interface_arm.move();
-  //movebot(move_group_interface_arm, -0.22972, -0.211, 0.348506, 0, 0.7071068, 0.7071068, 0);
   
 
   ros::WallDuration(1).sleep();
 
-  //movebot(move_group_interface_arm, -0.22881, -0.20941,0.180, 0, 0.7071068, 0.7071068, 0);
-  //movebot(move_group_interface_arm, -0.22881, -0.20941,0.173, 0, 0.7071068, 0.7071068, 0);
   //detachobj(planning_scene_diff_publisher,ps, 0);
 
   ros::WallDuration(1).sleep();
@@ -225,8 +195,3 @@ int main(int argc, char** argv)
   ros::shutdown();
   return 0;
 }
-/*
-cd ros/mammacheck/
-source devel/setup.bash
-killall -9 rosmaster; killall -9 rviz; killall gzserver; killall gzclient
-*/
